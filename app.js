2f7c2be400dddb022089d67f2b756ffe8c7127bc
Márcio Viegas <marcioaugustoviegas@gmail.com>
2020-08-02 18:13:19 -0300

    need to declare the plugin before the schema

--- app.js
+++ app.js
@@ -9,19 +9,16 @@ const Application = function () {
 	const SERVER_PORT = process.env.SERVER_PORT || 3000;
 	const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/xraytest?authSource=admin';
 	
+	mongoose.plugin(mongooseXray, {verbose: true});
+
 	let TestSchema = new mongoose.Schema({},  {strict: false});
 	
-	// The plugin works when attached to specifics Schemas, but not globally
-	// TestSchema.plugin(mongooseXray, {verbose: true});
-
 	let TestModel = mongoose.model('test', TestSchema);
 
 	// I could await, but mongo is always faster than server.listen - KISS
 	mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, auth: false}).then(() => {
 		console.log(`MongoDB connected on URI: ${MONGO_URI}`);
 	});
-
-	mongoose.plugin(mongooseXray, {verbose: true});
 
 	app.get('/', async (req, res) => {
 		return await new TestModel({}).save().then((r) => {
